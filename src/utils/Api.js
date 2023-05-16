f76b25653b3a389f51bf64513f0bb81f7f570e27
Alexey Krivchikov <krivchikovalexei@yandex.ru>
2023-05-16 14:24:13 +0300

    cred

--- src/utils/Api.js
+++ src/utils/Api.js
@@ -101,6 +101,7 @@ const api = new Api({
   headers: {
     // authorization: "13de05b0-eaab-40cb-aa9f-faea16b25706",
     "Content-Type": "application/json",
+    credentials: "include",
   },
 });
 
