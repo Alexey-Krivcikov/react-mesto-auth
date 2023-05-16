5e0b8a121324ce5a71930ca7df41d0b6490084b8
Alexey Krivchikov <krivchikovalexei@yandex.ru>
2023-05-16 13:44:24 +0300

    fix /

--- src/utils/Api.js
+++ src/utils/Api.js
@@ -90,7 +90,7 @@ class Api {
 
 const api = new Api({
   // baseUrl: "https://mesto.nomoreparties.co/v1/cohort-59",
-  baseUrl: "https://api.mesto.front.end.nomoredomains.monster/",
+  baseUrl: "https://api.mesto.front.end.nomoredomains.monster",
   headers: {
     // authorization: "13de05b0-eaab-40cb-aa9f-faea16b25706",
     "Content-Type": "application/json",
